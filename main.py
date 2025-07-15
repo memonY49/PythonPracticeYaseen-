626225ec3e343ea489cd9cb60ea38d2b5d80f262
memonY49 <memony49@gmail.com>
2025-07-15 18:35:30 +0500

    First Update

---
+++ main.py
@@ -0,0 +1,51 @@
+# print("Hello World")
+
+# Data type:
+    # 1. Premetive and 2.Non-Premetive (Primary and secondry)
+    # Primary:
+    # Int
+    # Float
+    # Char
+    # Boolean
+    # Secondry:
+    # String
+    # List
+# Naming Rules:
+    # 1. Name should never be started from a number.
+    # 2. Name should not contain any spaces in b/w.
+    # 3. Name should not be reserved words.
+    # 4. Name can only contains some accepted symbols. (_)
+# Vareable:
+    # It is a container that contains some data tha can be changed or modified
+    # Synatx of creating a variable:
+        # nameofvariable = 3
+
+# Operator:
+#       Arithmetic operators:
+#                     Add: +
+#                     Sub: -
+#                     Mul: *
+#                     Dev: /
+#                     Mod: %
+#      Conditional Operator:
+#                 less then: <
+#                 greater then: >
+#                 less then or equal to: <=
+#                                      : >=
+#                 equal to             : ==
+#                 not equal            : !=
+#     Logical  Operators:
+#                 And
+#                 Or
+#                 Not
+
+# How to take user input in python?
+#    use input() to take input from user.
+#    input() will always takes a string as an input.
+
+userin = int(input("Enter a number to print:"))
+
+print(userin-3)
+
+
+# Take two variables as an input from the user and perform all operators on those variables.
