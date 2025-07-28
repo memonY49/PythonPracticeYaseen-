7e82fc2fe273ce5ee5fdcbefeaee789bd5a87094
memonY49 <memony49@gmail.com>
2025-07-28 19:20:32 +0500

    Update

---
+++ listfunction.py
@@ -0,0 +1,31 @@
+# What is a fuction:
+#   Function is a block of code seperated from an atmosphare to perform specific task
+# list functions:
+#   append( Element to add)	Adds an element at the end of the list
+# 	clear()			Removes all the elements from the list
+# 	copy()			Returns a copy of the list
+# 	count(Element)		Returns the number of elements with the specified value
+# 	extend()		Add the elements of a list (or any iterable), to the end of the current list
+# 	index(Element)		Returns the index of the first element with the specified value
+# 	insert(Index, Element)	Adds an element at the specified position
+# 	pop()			Removes the element at the last element
+# 	remove(Element)		Removes the item with the specified value
+# 	reverse()		Reverses the order of the list
+# 	sort()			Sorts the list
+
+mylist = (7,2,6,2)
+
+mylist.append(7)
+
+# mylist2 = mylist.copy()
+
+# print(mylist)
+# print(mylist.count(2))
+# print(mylist.index(2))
+# mylist.pop(1)
+# mylist.remove(2)
+# mylist.reverse()
+
+# mylist.sort(reverse=True)
+
+print(mylist)
