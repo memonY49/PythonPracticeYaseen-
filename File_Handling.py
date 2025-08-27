832a5182040281b9ba384f9fe8c81f4ef39b1a41
memony49 <memony49@gmail.com>
2025-08-27 20:16:50 +0500

    update

--- File_Handling.py
+++ File_Handling.py
@@ -1,15 +1,21 @@
-file = open('data.txt','r')
-# file.write("yasir3,nawaz,03000000000,41999090939,abc@gmail.com,abc123\n")
-# print(file.readlines())
+# file = open('data.txt','r')
+# # file.write("yasir6,nawaz,03000000000,41999090939,abc@gmail.com,abc123\n")
+# # print(file.readline())
+# # print(file.readline())
+# #
+# # print(file.readlines())
+# # for line in file.readlines():
+# #     print(line.strip().split(',')[0])
+#
+# data = [line.strip().split(',') for line in file.readlines()]
+#
+# print(data)
+#
+#
+# file.close()
+
+with open('data.txt','r') as file:
+    print([lin.strip().split(',') for lin in file.readlines()])
 
 
-for line in file.readlines():
-    print(line.strip())
-
-
-
-
-
-file.close()
-
-
+print(file.read())
