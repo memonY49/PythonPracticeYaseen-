142cc3c8eaf5f0ef1c69b9d3761b95de48bd3e42
memony49 <memony49@gmail.com>
2025-08-28 21:30:11 +0500

    update

--- File_Handling.py
+++ File_Handling.py
@@ -14,8 +14,50 @@
 #
 # file.close()
 
-with open('data.txt','r') as file:
-    print([lin.strip().split(',') for lin in file.readlines()])
+# with open('data.txt','r') as file:
+#     print([lin.strip().split(',') for lin in file.readlines()])
 
 
-print(file.read())
+# print(file.read())
+
+
+# data = []
+#
+# with open("data.txt",'r') as file:
+#     data = [line.strip().split(',') for line in file.readlines()]
+#
+#
+# userEmail = input("Enter your email:")
+# userPass = input("Enter your password")
+# for user in data:
+#     if userEmail == user[4]:
+#
+#         if userPass == user[5]:
+#             print("Name:", user[0])
+#             print("FName:", user[1])
+#             print("Cnic:", user[3])
+#             print("Phone:", user[2])
+#             print("Email:", user[4])
+#             break
+#         else:
+#             print("Password is incorrect!")
+#             break
+# else:
+#     print("Email is incorrect!")
+
+
+# mode: a+,r+,w+
+
+with open("data2.txt",'a+') as file:
+    file.seek(0)
+    # print(file.read())
+    file.write('Yasir6,nawaz,03000000000,41999090939,abc5@gmail.com,abc123\n')
+
+
+
+
+
+
+
+
+
